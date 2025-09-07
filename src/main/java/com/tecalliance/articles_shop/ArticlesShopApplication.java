c1c095e26aaf39c6c89cb0239c888b99f4a06dbb
ChauNgo1106 <baochaust@gmail.com>
2025-09-07 10:14:32 +0700

    Maven setup

---
+++ src/main/java/com/tecalliance/articles_shop/ArticlesShopApplication.java
@@ -0,0 +1,13 @@
+package com.tecalliance.articles_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesShopApplication.class, args);
+	}
+
+}
